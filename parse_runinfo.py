a418e210cb4ef4164d46958f9f7081ac2a78b86f
Robert Ernst <rfernst@gmail.com>
2014-11-17 09:54:14 +0100

    Parsing first HS fields

--- parse_runinfo.py
+++ parse_runinfo.py
@@ -4,9 +4,33 @@ from xml.dom.minidom import parse
 from app import db
 from app.models import RunInfo, Platform
 
+### Use zip?
+
+
+#get_firstChild_value
+def get_firstChild_value(run_param, tag):
+    return
+
 #Parse HiSeq
-def parse_hiseq(run_parameters):
-    print "Hiseq\n"
+def parse_hiseq(run_params):
+    run_id = run_params.getElementsByTagName('RunID')[0].firstChild.nodeValue
+    experiment_name = run_params.getElementsByTagName('ExperimentName')[0].firstChild.nodeValue
+
+    run_mode = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    barcode = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    run_start_date = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    pair_end_fc = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    read_1 = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    read_2 = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    index_read_1 = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    index_read_2 = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+    pe = run_params.getElementsByTagName('')[0].firstChild.nodeValue
+
+    HSfields = ['RunMode', 'Barcode','RunStartDate','PairEndFC','Read1','Read2', 'IndexRead1','IndexRead2','Pe' ]
+
+
+
+    #run_info = RunInfo(run_id, experiment_name)
 
 #Parse MiSeq
 #Parse NextSeq
@@ -16,11 +40,11 @@ if __name__ == "__main__":
     parser.add_argument('platform', choices=['hiseq','miseq','nextseq'], help="Sequencing platform")
     parser.add_argument('paramFile', help="Illumina runParameters.xml file")
     args = parser.parse_args()
-    
+
     # Parse runParameters.xml
     run_parameters = parse(args.paramFile)
     platform = args.platform
-    
+
     if platform == 'hiseq':
 	run_info = parse_hiseq(run_parameters)
     #elif platform == 'miseq':
@@ -30,11 +54,12 @@ if __name__ == "__main__":
 
     #db.session.add(run_info)
     #db.session.commit()
-    
+
 """
+    HSfields = ['ApplicationName','RunID','ExperimentName','RunMode', 'Barcode','RunStartDate','PairEndFC','Read1','Read2', 'IndexRead1','IndexRead2','Pe' ]
     #if run_parameters.getElementsByTagName('ApplicationName')[0].firstChild.nodeValue == 'MiSeq Control Software':
 	platform = 'miseq'
-    
+
     elif runParameters.getElementsByTagName('ApplicationName')[0].firstChild.nodeValue == 'HiSeq Control Software':
     #typically HiSeq format
     result = ''
